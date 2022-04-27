35e674ba3c1bdee0e5536a016f699771d4f329bc
Akai <59208058+SuuPS@users.noreply.github.com>
2022-04-27 19:47:59 +0600

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import logo from './logo.svg';
+import './App.css';
+import Header from './components/header';
+import Navbar from './components/navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
